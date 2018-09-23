e11dac3a658faabfbf2f496399ae71ab5e0c1a0b
Jouny <dz4tune@gmail.com>
2018-09-23 18:53:27 +0300

    fibonacci method

--- findmin.py
+++ findmin.py
@@ -30,23 +30,24 @@ class Counter:
 
 
 def findmin(func, borders, accuracy):
-    yield borders
     delta = accuracy / 3
     a, b = borders
     while b - a > accuracy:
         x = (a + b) / 2
         x1, x2 = x - delta, x + delta
+        yield a, b,x1,x2
         fx1, fx2 = func(x1), func(x2)
         if fx1 > fx2:
             a = x1
         elif fx1 < fx2:
             b = x2
-        yield a, b
 
 def golden_ratio(func, borders, accuracy):
     a, b = borders
-    x1 = a + (3 - 5**.5)/2 * (b-a)
-    x2 = a + (5**.5 - 1)/2 * (b-a)
+    leftGL = (3 - 5**.5)/2
+    rightGR = (5**.5 - 1)/2
+    x1 = a + leftGL * (b-a)
+    x2 = a + rightGR * (b-a)
     yield a,b,x1,x2
     fx1 = func(x1)
     fx2 = func(x2)
@@ -55,15 +56,51 @@ def golden_ratio(func, borders, accuracy):
             a = x1
             x1 = x2
             fx1 = fx2
-            x2 = a + (5**.5 - 1)/2 * (b-a)
+            x2 = a + rightGR * (b-a)
             fx2 = func(x2)
         elif fx2 > fx1:
             b = x2
             x2 = x1
             fx2 = fx1
-            x1 = a + (3 - 5**.5)/2 * (b-a)
+            x1 = a + leftGL * (b-a)
             fx1 = func(x1)
         yield a,b,x1,x2
+
+def fib(n):
+    return int((((1+5**.5)/2)**n-((1-5**.5)/2)**n)/5**.5)
+
+def find_fib(borders, accuracy):
+    position = 10
+    a, b = borders
+    x = (a-b)/accuracy
+    while fib(position) <= x:
+        position *= 2
+    return position
+
+def fib_search(func, borders, accuracy):
+    N = find_fib(borders, accuracy)
+    fibN2, fibN1 = fib(N), fib(N-1)
+    fibN  = fibN2 - fibN1
+    a,b = borders
+    c = (b-a)/fibN2
+    x1, x2 = a + fibN * c, a + fibN1 * c
+    fx1, fx2 = func(x1), func(x2)
+    yield a,b,x1,x2
+    while True:
+        fibN2, fibN1 = fibN1, fibN
+        fibN = fibN2 - fibN1
+        c = (b-a)/fibN2
+        if fx1 < fx2:
+            b = x2
+            x2, fx2 = x1, fx1
+            x1 = a + fibN * c
+            fx1 = func(x1)
+        elif fx1 > fx2:
+            a = x1
+            x1, fx1 = x2, fx2
+            x2 = a + fibN1 * c
+            fx2 = func(x2)
+        yield a, b, x1, x2
 
 
 if __name__ == '__main__':
