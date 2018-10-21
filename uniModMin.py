512915c71d32f831d6d7471d598f6adfd11b65dd
Jouny <dz4tune@gmail.com>
2018-10-21 16:08:30 +0300

    left only gradient descent

--- uniModMin.py
+++ uniModMin.py
@@ -97,6 +97,9 @@ def findmin(func: Cff, borders: Tff, accuracy: float):
     >>> res = findmin(lambda x: (x-3)**2 + 1, (-100, 100), .001)
     >>> 2.999 < res < 3.001
     True
+    >>> res = findmin(lambda x: (x+10)**2 + 1, (-100, 100), .001)
+    >>> -10.001 < res < -9.999
+    True
     """
     for a, b, x1, x2 in __findmin(func, borders, accuracy):
         pass
