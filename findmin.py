09a8d0074b2847b957323098f981d629db8f203b
Jouny <dz4tune@gmail.com>
2018-10-21 14:01:46 +0300

    left only gradient descent

--- findmin.py
+++ findmin.py
@@ -1,35 +1,14 @@
 #!/usr/bin/env python
 from matplotlib import pyplot
 from matplotlib.widgets import Button
-from matplotlib.lines import Line2D
 from mpl_toolkits.mplot3d import proj3d
 from typing import Iterable, Generator, Tuple, Callable
 import numpy
-import itertools
-import uniModMin
-from counter import IterCounter
+from uniModMin import in_direction_of, findmin, find_interval
+from numpy.linalg import norm
+from copy import copy
 
 vec = numpy.ndarray
-
-
-def update_all_x_data(lines: Iterable[Line2D], gen: Generator[Tuple, None, None]) -> Generator[None, None, None]:
-    for args in gen:
-        for line, xdata in zip(lines, args):
-            line.set_xdata(xdata)
-        yield args
-
-
-def add_to_line(axis, start, changes):
-    for grad in changes:
-        next_start = start + -grad
-        axis.plot(*zip(start, next_start), [foo(start), foo(next_start)], 'r-')
-        start = next_start
-        yield start
-
-
-def flatten(iterable: Iterable[Iterable]) -> Iterable:
-    for i in iterable:
-        yield from i
 
 
 def gradient(func: Callable[[numpy.ndarray], float], x: numpy.ndarray, delta: float) -> numpy.ndarray:
@@ -39,86 +18,58 @@ def gradient(func: Callable[[numpy.ndarray], float], x: numpy.ndarray, delta: fl
     >>> gradient(lambda x:x[0]*2+x[1]*3, numpy.array([1.0, 4.2]),.1)
     array([2., 3.])
     '''
-    fx = func(x)
     result = []
     for index in range(len(x)):
-        x[index] += delta
-        result.append((func(x) - fx) / delta)
+        x[index] += delta/2
+        right = func(x)
         x[index] -= delta
+        left = func(x)
+        x[index] += delta/2
+        result.append((right - left) / delta)
     return numpy.array(result)
 
 
-def gradient_descent(func: Callable[[vec], float], x: vec, delta: float = .001, step: float = .001):
-    while True:
-        grad = gradient(func, x, delta)
-        grad /= max(max(grad), abs(min(grad)))
-        next_x = x + step * -grad
-        if all(map(lambda xi: xi <= delta, x)):
-            break
-        x = next_x
-        yield grad
+def gradient_descent(func: Callable[[vec], float], start: vec, delta: float = .001):
+    step = numpy.array([delta + 1])
+    while all(abs(step) > delta):
+        step = find_step(func, start, delta)
+        start += step
+        yield start
 
 
-class DrawOnClick:
-    def __init__(self, figure, gen):
-        self.figure = figure
-        self.gen = gen
-        self.last = None
-
-    def __call__(self, event):
-        try:
-            self.last = next(self.gen)
-        except StopIteration:
-            pass
-        else:
-            self.figure.canvas.flush_events()
-            self.figure.canvas.draw()
+def find_step(func: Callable, position, delta):
+    grad = -gradient(func, position, delta)
+    func_slice = in_direction_of(func, position, grad)
+    step = findmin(func_slice, find_interval(func_slice, 0), delta)
+    return grad / norm(grad) * step
 
 
 if __name__ == '__main__':
-    def f(x):
-        return x ** 2 + 3 * x
-
     def foo(x):
         x1, x2 = x[0], x[1]
         return 100 * (x2 - x1 ** 2) ** 2 + (1 - x1) ** 2
 
-    accuracy = .005
-    left, right = -10, 10
-    arr = numpy.arange(left, right, accuracy / 10)
-
-    pyplot.ion()
     fig = pyplot.figure()
-    ax = fig.add_subplot(1, 2, 1)
 
-    x = y = numpy.linspace(-10, 10, 20)
-    X, Y = numpy.meshgrid(x, y)
-    Z = foo((X, Y))
-    ax3d = fig.add_subplot(1, 2, 2, projection='3d')
-    ax3d.plot_surface(X, Y, Z)
+    x = numpy.linspace(-100, 100, 2000)
+    X, Y = numpy.meshgrid(x, x)
+    start = numpy.array([80.0, -70.0])
+    ax3d = fig.add_subplot(1, 1, 1, projection='3d')
+    ax3d.plot_surface(X, Y, foo((X, Y)))
     ax3d.set_xlabel('X1')
     ax3d.set_ylabel('X2')
-    desc = IterCounter(gradient_descent(foo, numpy.array([7.0, 7.0]), .001, 1))
-    g = add_to_line(ax3d, numpy.array([7.0, 7.0]), desc)
     bnext3d = Button(pyplot.axes([.9, 0, 1, .1]), 'Next')
-    clicker1 = DrawOnClick(fig, g)
-    bnext3d.on_clicked(clicker1)
+    desc = gradient_descent(foo, start, .001)
+    prev = copy(start)
 
-    line1 = ax.plot(arr, f(arr), 'b-')
-    _, _, *yborders = ax.axis()
-    coords = (0, 0), yborders
-    lines = ax.plot(
-        *coords, 'g-',
-        *coords, 'r-',
-        *coords, 'g-.',
-        *coords, 'r-.',
-    )
-    func = uniModMin.fib_search
-    counter_gen = IterCounter(func(f, (left, right), accuracy))
-    gen = update_all_x_data(lines, counter_gen)
-    bnext = Button(pyplot.axes([0, 0, .1, .1]), 'Next')
-    clicker2 = DrawOnClick(fig, gen)
-    bnext.on_clicked(clicker2)
+
+    def on_click(event):
+        global prev
+        point = next(desc)
+        ax3d.plot(*zip(prev, point), [foo(prev), foo(point)], 'r-')
+        prev = copy(point)
+
+    bnext3d.on_clicked(on_click)
+
     pyplot.show(block=True)
-    print(f'Gradient descent:\n\tCalls:{desc.count}\n\tresult:{clicker1.last}')
-    print(f'2d func:\n\tCalls:{counter_gen.count}\n\tresult:{clicker2.last}')
+    print(prev, foo(prev))
